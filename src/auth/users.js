c583d767bb2b350d4fbff5a81c537143af1f24e4
Nakshatra Tiwari <naka@Nakshatras-MacBook-Pro.local>
2026-07-06 14:39:00 +0530

    Move users changes to auth users

--- src/auth/users.js
+++ src/auth/users.js
@@ -2,20 +2,111 @@
 // src/auth/users.js
 
 export const users = [
+  
   {
-    username: "Naka",
+    username: "Pankaj",
+    password: "0402"
+  },
+
+  {
+    username: "Bharat",
+    password: "0000"
+  },
+
+  {
+    username: "Akansha",
+    password: "1111"
+  },
+
+  {
+    username: "Tamanna",
+    password: "1504"
+  },
+
+  {
+    username: "Rajesh",
+    password: "1904"
+  },
+
+  {
+    username: "Vishal",
+    password: "0405"
+  },
+
+  {
+    username: "Barkat",
+    password: "2009"
+  },
+
+  {
+    username: "Bharat",
+    password: "0000"
+  },
+
+  {
+    username: "Dr Jyoti",
+    password: "1906"
+  },
+
+  {
+    username: "Rehman",
+    password: "0704"
+  },
+
+  {
+    username: "Chetna",
+    password: "2107"
+  },
+
+  {
+    username: "Shweta",
+    password: "2222"
+  },
+
+  {
+    username: "Shreyansh",
+    password: "1208"
+  },
+
+  {
+    username: "Murad",
+    password: "3333"
+  },
+
+  {
+    username: "Nakshatra",
     password: "0509"
   },
 
   {
-    username: "Priya",
-    password: "horm123"
+    username: "Dr Suvarna",
+    password: "0709"
   },
 
   {
-    username: "Arun",
-    password: "hema123"
-  }
+    username: "Dr Sumer",
+    password: "1509"
+  },
+  {
+    username: "Naresh",
+    password: "0210"
+  },
+
+  {
+    username: "Rahul",
+    password: "2610"
+  },
+
+  {
+    username: "Ankita",
+    password: "2710"
+  },
+
+
+
+
+
+
 ];
 
 export const departments = [
@@ -55,8 +146,6 @@ export const departments = [
     url: "/index_outsource.html"
   },
 
-
-
   {
     name: "Validator",
     url: "/index_validator.html"
@@ -65,6 +154,11 @@ export const departments = [
   {
     name: "Critical Alerts",
     url: "/Critical.html"
+  },
+
+  {
+    name: "Inventory Intake",
+    url: "/inventory.html"
   }
 
 ];
