0bd868124d9535d5d8220dc1c16ba0360d231577
Nakshatra Tiwari <naka@Nakshatras-MacBook-Pro.local>
2026-07-06 12:50:21 +0530

    Add critical entry point

---
+++ src/main_critical.jsx
@@ -0,0 +1,10 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import CriticalAlertDashboard from "./critical/CriticalAlertDashboard.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <CriticalAlertDashboard />
+  </React.StrictMode>
+);
